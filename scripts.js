14a9ab4be18df4583203e83cff1b4367df72f30c
Caio Fernandes <caio_fsouza@hotmail.com>
2016-07-12 12:15:26 -0300

    refactor some js functions and download imgs and gif to faster loading page

--- scripts.js
+++ scripts.js
@@ -1,90 +1,93 @@
 loadJS("https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js");
 loadJS("https://cdn.rawgit.com/mikeflynn/egg.js/master/egg.min.js");
 
-var egggif = document.createElement("img");
-egggif.src = "http://media.giphy.com/media/4hnQDVKVARZ6w/giphy.gif";
-egggif.id = "egggif";
-egggif.style.display = "none";
+// konami code
+var options = {
+    src: "imgs/konami-code.gif",
+    id: "egggif",
+    styles:{
+        display: "none",
+        position: "absolute",
+        top: "33%",
+        left: "25%"
+    }
+};
+loadImgTag(options);
 
-var egggif2 = document.createElement("img");
-egggif2.src = "http://i.imgur.com/YsbKHg1.gif";
-egggif2.id = "egggif2";
-egggif2.style.display = "none";
+// magic
+options.src = "imgs/magic.gif";
+options.id = "egggif2";
+options.styles = {
+    display: "none",
+    position: "absolute",
+    top: "33%",
+    left: "35%"
+};
+loadImgTag(options);
 
-var egggif3d = document.createElement("img");
-egggif3d.src = "http://barkpost-assets.s3.amazonaws.com/wp-content/uploads/2013/11/3dDoge.gif";
-egggif3d.id = "egggif3d";
-egggif3d.style.display = "none";
+
+// 3d touch
+options.src = "imgs/3Ddog.gif";
+options.id = "egggif3d";
+options.styles = {
+    display: "none",
+    position: "absolute",
+    top: "25%",
+    left: "40%"
+};
+loadImgTag(options);
 
 
+// dinofauro
 var egggif4_container = document.createElement("div");
 egggif4_container.id = 'egggif4_container';
+egggif4_container.style.display = "none";
+egggif4_container.style.position = "absolute";
+egggif4_container.style.bottom = "0";
+egggif4_container.style.right = "-300px";
+egggif4_container.style.width = "300px";
+egggif4_container.style.display = "none";
+
 var egggif4_title = document.createElement("h1");
 egggif4_title.innerHTML = "ICOLAFORA";
 egggif4_title.style.color = "orange";
 egggif4_title.style.fontSize = "50px";
 egggif4_title.style.fontWeight = "bold";
-
 egggif4_container.appendChild(egggif4_title);
-egggif4_container.style.display = "none";
-
 
 var egggif4 = document.createElement("img");
-egggif4.src = "http://hojeemdia.com.br/polopoly_fs/1.184830!/image/image.jpg_gen/derivatives/landscape_653/image.jpg";
+egggif4.src = "imgs/dinofauro.jpg";
 egggif4.id = "egggif4";
-egggif4.style.width = "300px";
+egggif4.style.width = "250px";
+egggif4.style.height = "250px";
 egggif4_container.appendChild(egggif4);
-
-
-var egg_jequiti = document.createElement("img");
-egg_jequiti.src = "https://pbs.twimg.com/profile_images/461593982661718016/fr1v4YpS.jpeg";
-egg_jequiti.id = "egg_jequiti";
-egg_jequiti.style.display = "none";
-
-
-var egggif5 = document.createElement("img");
-egggif5.src = "http://1.bp.blogspot.com/-o9MGphc_jgw/UVSo11dql8I/AAAAAAAALIo/IWWSBoxTDFg/s1600/thumbs-up.gif";
-egggif5.id = "egggif5";
-egggif5.style.display = "none";
-
-
-document.getElementsByTagName("body")[0].appendChild(egggif);
-document.getElementsByTagName("body")[0].appendChild(egggif2);
-document.getElementsByTagName("body")[0].appendChild(egggif3d);
 document.getElementsByTagName("body")[0].appendChild(egggif4_container);
-document.getElementsByTagName("body")[0].appendChild(egg_jequiti);
-document.getElementsByTagName("body")[0].appendChild(egggif5);
+
+
+// jequiti
+options.src = "imgs/jequiti.jpeg";
+options.id = "egg_jequiti";
+options.styles = {
+    display: "none",
+    position: "absolute",
+    width: "100%",
+    height: "100%"
+};
+loadImgTag(options);
+
+// ta pronto
+options.src = "imgs/tapronto.gif";
+options.id = "egggif5";
+options.styles = {
+    display: "none",
+    position: "absolute",
+    top: "33%",
+    left: "25%"
+};
+loadImgTag(options);
+
 
 window.onload = function(){
-
-    $("#egggif, #egggif2, #egggif5").css({
-        "position": "absolute",
-        "top": "33%",
-        "left": "25%",
-        "display": "none"
-    });
-
-    $("#egggif3d").css({
-        "position": "absolute",
-        "top": "25%",
-        "left": "40%",
-        "display": "none"
-    });
-
-    $("#egggif4_container").css({
-      "position": "absolute",
-      "bottom": "0px",
-      "right": "-300px",
-      "width": "300px",
-      "display": "none"
-    });
-
-    $("#egg_jequiti").css({
-        "position": "absolute",
-        "display": "none",
-        "width": "100%",
-        "height": "100%"
-    });
 
     var egg = new Egg();
     egg.addCode("up,up,down,down,left,right,left,right,b,a", function() {
@@ -106,21 +109,21 @@ window.onload = function(){
     egg.addCode("d,i,n,o,f,a,u,r,o", function() {
         $('#egggif4_container').show(200).animate({
             "right": "110%"
-        }, 5000);
+        }, 3500);
         window.setTimeout(function () {
 
             $('#egggif4_container').hide(100).css({
-              "right": "-300px"
+                "right": "-300px"
             });
         }, 5000);
     }, "dinofauro");
 
     egg.addCode("j,e,q,u,i,t,i", function() {
-      $('#egg_jequiti').show(5, function() {
-          window.setTimeout(function() {
-            $('#egg_jequiti').hide();
-          }, 5);
-      });
+        $('#egg_jequiti').show(5, function() {
+            window.setTimeout(function() {
+                $('#egg_jequiti').hide();
+            }, 500);
+        });
     }, "jequiti");
 
     egg.addCode("t,a,p,r,o,n,t,o", function() {
@@ -140,9 +143,9 @@ window.onload = function(){
     var easter_egg = new MyEasterEgg();
     easter_egg.code = function () {
         $('#egggif3d').fadeIn(500, function() {
-          window.setTimeout(function () {
-            $('#egggif3d').hide();
-          }, 5000);
+            window.setTimeout(function () {
+                $('#egggif3d').hide();
+            }, 5000);
         });
     }
     easter_egg.load();
@@ -158,77 +161,87 @@ function loadJS(src_js){
     head.appendChild(newJS);
 }
 
+function loadImgTag(options){
+    var imgTag = document.createElement("img");
+    imgTag.src = options.src;
+    imgTag.id = options.id;
+    for(prop in options.styles){
+        imgTag.style[prop] = options.styles[prop];
+    }
+    document.getElementsByTagName("body")[0].appendChild(imgTag);
+}
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // Easter Egg que funfa com touch
 var MyEasterEgg = function (callback) {
-  var my_egg = {
-    addEvent: function (obj, type, fn, ref_obj) {
-      if (obj.addEventListener) {
-        obj.addEventListener(type, fn, false);
-      }
-    },
-    load: function (link) {
-      this.touch_screen.load(link);
-    },
-    code: function (link) {
-      window.location = link
-    },
-    touch_screen: {
-      start_x: 0,
-      start_y: 0,
-      stop_x: 0,
-      stop_y: 0,
-      tap: false,
-      capture: false,
-      orig_keys: "",
-      keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
-      code: function (link) {
-        my_egg.code(link);
-      },
-      load: function (link) {
-        this.orig_keys = this.keys;
-        my_egg.addEvent(document, "touchmove", function (e) {
-          if (e.touches.length == 1 && my_egg.touch_screen.capture == true) {
-            var touch = e.touches[0];
-            my_egg.touch_screen.stop_x = touch.pageX;
-            my_egg.touch_screen.stop_y = touch.pageY;
-            my_egg.touch_screen.tap = false;
-            my_egg.touch_screen.capture = false;
-            my_egg.touch_screen.check_direction();
-          }
-        });
-        my_egg.addEvent(document, "touchend", function (evt) {
-          if (my_egg.touch_screen.tap == true) my_egg.touch_screen.check_direction(link);
-        }, false);
-        my_egg.addEvent(document, "touchstart", function (evt) {
-          my_egg.touch_screen.start_x = evt.changedTouches[0].pageX;
-          my_egg.touch_screen.start_y = evt.changedTouches[0].pageY;
-          my_egg.touch_screen.tap = true;
-          my_egg.touch_screen.capture = true;
-        });
-      },
-      check_direction: function (link) {
-        x_magnitude = Math.abs(this.start_x - this.stop_x);
-        y_magnitude = Math.abs(this.start_y - this.stop_y);
-        x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
-        y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
-        result = (x_magnitude > y_magnitude) ? x : y;
-        result = (this.tap == true) ? "TAP" : result;
+    var my_egg = {
+        addEvent: function (obj, type, fn, ref_obj) {
+            if (obj.addEventListener) {
+                obj.addEventListener(type, fn, false);
+            }
+        },
+        load: function (link) {
+            this.touch_screen.load(link);
+        },
+        code: function (link) {
+            window.location = link
+        },
+        touch_screen: {
+            start_x: 0,
+            start_y: 0,
+            stop_x: 0,
+            stop_y: 0,
+            tap: false,
+            capture: false,
+            orig_keys: "",
+            keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
+            code: function (link) {
+                my_egg.code(link);
+            },
+            load: function (link) {
+                this.orig_keys = this.keys;
+                my_egg.addEvent(document, "touchmove", function (e) {
+                    if (e.touches.length == 1 && my_egg.touch_screen.capture == true) {
+                        var touch = e.touches[0];
+                        my_egg.touch_screen.stop_x = touch.pageX;
+                        my_egg.touch_screen.stop_y = touch.pageY;
+                        my_egg.touch_screen.tap = false;
+                        my_egg.touch_screen.capture = false;
+                        my_egg.touch_screen.check_direction();
+                    }
+                });
+                my_egg.addEvent(document, "touchend", function (evt) {
+                    if (my_egg.touch_screen.tap == true) my_egg.touch_screen.check_direction(link);
+                }, false);
+                my_egg.addEvent(document, "touchstart", function (evt) {
+                    my_egg.touch_screen.start_x = evt.changedTouches[0].pageX;
+                    my_egg.touch_screen.start_y = evt.changedTouches[0].pageY;
+                    my_egg.touch_screen.tap = true;
+                    my_egg.touch_screen.capture = true;
+                });
+            },
+            check_direction: function (link) {
+                x_magnitude = Math.abs(this.start_x - this.stop_x);
+                y_magnitude = Math.abs(this.start_y - this.stop_y);
+                x = ((this.start_x - this.stop_x) < 0) ? "RIGHT" : "LEFT";
+                y = ((this.start_y - this.stop_y) < 0) ? "DOWN" : "UP";
+                result = (x_magnitude > y_magnitude) ? x : y;
+                result = (this.tap == true) ? "TAP" : result;
 
-        if (result == this.keys[0]) this.keys = this.keys.slice(1, this.keys.length);
-        if (this.keys.length == 0) {
-          this.keys = this.orig_keys;
-          this.code(link);
+                if (result == this.keys[0]) this.keys = this.keys.slice(1, this.keys.length);
+                if (this.keys.length == 0) {
+                    this.keys = this.orig_keys;
+                    this.code(link);
+                }
+            }
         }
-      }
     }
-  }
 
-  typeof callback === "string" && my_egg.load(callback);
-  if (typeof callback === "function") {
-    my_egg.code = callback;
-    my_egg.load();
-  }
+    typeof callback === "string" && my_egg.load(callback);
+    if (typeof callback === "function") {
+        my_egg.code = callback;
+        my_egg.load();
+    }
 
-  return my_egg;
+    return my_egg;
 };
